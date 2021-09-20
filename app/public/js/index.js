c45c5bf9ec178ac61a81f41362b91e9c5122a438
Parth Patekar <parth.15je001620@am.ism.ac.in>
2021-09-20 01:48:01 -0400

    integrated randomuser api with the website to dynamically generate user data with every refresh

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,11 +1,35 @@
-const Offer = {
+const User = {
     data() {
-        return {
-            foo: 0,
-            msg: "D&S is my favourite",
-            list: ["red", "blue", "green"]
+        return{
+            result: {
+                name: "",
+                location: "",
+                dob: "",
+                picture: ""
+            }
         }
+    },
+    methods: {
+        format_date(date_string) {
+            const date_ = new Date(date_string);
+            return new Intl.DateTimeFormat('default', {dateStyle: 'long'}).format(date_);
+        }
+    },
+    created() {
+        fetch('https://randomuser.me/api')
+        .then(response => response.json())
+        .then(
+            (json) => {                                
+                this.result = json.results[0];                
+                console.log(this.result);
+            }
+        )
+        .catch(
+            function(error) {
+                console.error(error);
+            }
+        )
     }
 }
 
-Vue.createpp(Offer).mount('#offer')
+Vue.createApp(User).mount('#user')
